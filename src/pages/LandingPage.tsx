3fd90b518fa3b05f7249d39ede09eb49a29315de
ptt98hd <thanhpham.hd.98@gmail.com>
2026-02-16 20:50:14 +0700

    feat: add landing page with hero, featured cars, and services sections.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -44,7 +44,7 @@ export function LandingPage() {
       <Navbar />
       <main className='flex-1'>
         {/* Hero Section */}
-        <section className='space-y-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32'>
+        <section className='from-primary/10 space-y-6 border-b bg-gradient-to-b via-transparent to-transparent pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32'>
           <div className='container mx-auto flex max-w-[64rem] flex-col items-center gap-4 text-center'>
             <h1 className='font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl'>
               Rent the car of your dreams.
